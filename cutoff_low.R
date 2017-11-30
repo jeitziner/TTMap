2b18911797b1a15066c8940d2f2ed3f51417aea6
jeitziner <32093941+jeitziner@users.noreply.github.com>
2017-11-30 09:13:43 +0100

    Delete cutoff_low.R

--- cutoff_low.R
+++
@@ -1,9 +0,0 @@
-cutoff_low <-
-function(low_map,para=1,text = "low"){
-if(dim(low_map)[1]==1){if(low_map==0){
-print(paste(paste("There were not enough",text, sep = " "),
-"classified samples",sep=" "))}}
-else{s<- apply(low_map,1,sum)
-low_map <- low_map[s>para,]}
-return(low_map);
-}
