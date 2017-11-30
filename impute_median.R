c7de49aadee11c531f0c22ab1fde71e095d89c40
jeitziner <32093941+jeitziner@users.noreply.github.com>
2017-11-30 09:09:45 +0100

    Delete impute_median.R

--- impute_median.R
+++
@@ -1,8 +0,0 @@
-impute_median <-
-function(ma,med){
-ma <- t(apply(ma,1,function(ma) ifelse(is.na(ma), 
-median(ma, na.rm=TRUE), ma)))
-ma[is.na(rowSums(ma))==TRUE,]<-
-med[is.na(rowSums(ma))==TRUE]
-return(ma)
-}
