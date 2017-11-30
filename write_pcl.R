33abbb134eb5bbcc3967875315cd93b689a9fd00
jeitziner <32093941+jeitziner@users.noreply.github.com>
2017-11-30 09:11:54 +0100

    Delete write_pcl.R

--- write_pcl.R
+++
@@ -1,8 +0,0 @@
-write_pcl <-
-function(df,dataname,fileaddress=""){
-dir.address <- paste(fileaddress,
-dataname,".pcl",sep="");
-X <- write.table(df,file=dir.address,
-append=FALSE,quote=FALSE,sep="\t",eol="\n",na="",
-row.names=FALSE,col.names=TRUE);
-return(X)}
